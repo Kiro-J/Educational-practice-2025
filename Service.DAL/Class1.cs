1e5ca11c76eae890d31dd76a9c200a4b141c4005
Moroshan1611 <okhlopovskiynikolay@mail.ru>
2025-11-03 16:05:30 +0200

    04.11.2025
    Редакция от 04.11

---
+++ Service.DAL/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Service.DAL
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Service/wwwroot/js/shared_script.js
@@ -0,0 +1,9 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    window.addEventListener('scroll', function () {
+        var header= document.getElementById('header-top');
+        var scrollTop = window.scrolly;
+        var maxScroll = 250;
+        var opacity=Math.min(scrollTop / max5croll, 1);
+        header.style.backgroundColor = `rgba(255, 165, 8, $(opacity))`;
+    });
+});
